d98bf93192830211edc02ec4f44bf609499c2964
digital-idoru <performanceof100@gmail.com>
2015-11-15 00:34:09 -0500

    Setting up initial steps.

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "grammars.h"
 
 #DEFINE debug 1
 
 
+
 int main(int argc, const char** argv) {
 
+  int string_size = 0;
+  char* input_string = NULL;
+  char** Table = NULL; 
+  int i, j = 0;
+  
+  printf("Please enter a string size: ");
+  scanf("%d", &string_size);
+
+  printf("Creating CYK Table.\n");
+  
 
 
   return 0;
+
 }
